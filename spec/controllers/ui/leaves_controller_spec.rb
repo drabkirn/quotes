c473ff2196cbedb243075c8e8aca3c2b8dab0d19
Aditya Nagla <me@cdadityang.xyz>
2019-12-21 05:49:53 +0000

    V1.0.4: Writing quote model spec

--- spec/controllers/ui/leaves_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Ui::LeavesController, type: :controller do
-
-end
--- spec/factories/quotes.rb
+++ spec/factories/quotes.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :quote do
-    title { "MyString" }
-    content { "MyText" }
-    author { "MyString" }
-    tags { "MyText" }
+    title { Faker::Lorem.sentence(word_count: 5) }
+    content { Faker::Lorem.sentence(word_count: 12) }
+    author { Faker::Name.name }
+    tags { Faker::Lorem.words(number: 4) }
+    #=> ["culpa", "recusandae", "aut", "omnis"]
   end
 end
