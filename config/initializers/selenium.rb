730c05e5e8ac2a8eafea838193a73ce0ac9eb558
Aditya Nagla <me@cdadityang.xyz>
2019-12-24 05:29:09 +0000

    V1.0.15: Selenium production fix

--- config/initializers/selenium.rb
+++ config/initializers/selenium.rb
@@ -1,6 +1,8 @@
-require "rubygems"
-require "selenium-webdriver"
+if Rails.env.development? or Rails.env.test?
+  require "rubygems"
+  require "selenium-webdriver"
 
-Selenium::WebDriver.logger.level = :debug
-Selenium::WebDriver.logger.output = '/tmp/selenium.log'
-ENV['DISPLAY']=':99.0'
+  Selenium::WebDriver.logger.level = :debug
+  Selenium::WebDriver.logger.output = '/tmp/selenium.log'
+  ENV['DISPLAY']=':99.0'
+end
